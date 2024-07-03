1908adcf673e21a2bb86ff4d575535511572232f
nishchay-v <nishchayvashistha@gmail.com>
2024-07-03 11:31:58 +0530

    personalisation changes

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Nishchay Vashistha',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+    'Nishchay Vashistha is a full-stack developer who loves to build digital experiences that are fast, beautiful, and accessible.',
+  url: 'https://nishchay-v-site.dev',
 };
