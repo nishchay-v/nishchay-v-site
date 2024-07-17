b17c11f587ec6b530bce9580ddfaaf9ba5a7e308
nishchay-v <nishchayvashistha@gmail.com>
2024-07-17 23:39:52 +0530

    chore: enable ignoreBuildErrors for TypeScript in next.config.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,14 @@ const nextConfig = {
   //   ],
   // },
 
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
